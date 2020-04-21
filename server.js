a9a8d45716d85c788553eb541394662b0073b701
giacomo9999 <gmarchesi888@gmail.com>
2020-04-21 11:55:20 -0400

    Setting up routes.

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+require("dotenv").config();
+const app = require("./app");
+const PORT = process.env.PORT;
+
+const server = http.createServer(app);
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
